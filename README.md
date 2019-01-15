0a4050f0159a60c232f443b7e16d39cdc2f8148f
YSIM <ysim@support@vk.com>
2019-01-15 03:49:54 +0300

    release v1 and last

---
+++ README.md
@@ -0,0 +1,32 @@
+# port_scan_for_webcam
+This is script for scaning ips working with ip-cam.
+After starting the script displays the result of execution in the terminal. 
+In fact, you can use any ports.
+
+
+For run script:
+1) Create the file "ips.txt".
+2) Run as root or disable password for root.
+3) Download if not nmap, grep, awk. Maybe sudo apt install nmap, grep, awk ?
+
+Functional:
+After entering the number of ports from 1 to 4, if you want to use more ports, enter “more”. If you entered "more", specify the number of ports to scan, then specify the port numbers. The result will be the work will be in the terminal and in the file result.txt.
+
+Recommendations:
+
+To date, 01/15/2019:
+
+XM/JUFENG - Port numbers: TCP port (34567), HTTP port (80), ONVIF port (8899).
+HIKVISION - Port numbers: HTTP port (80), RTSP port (554), HTTPS (443), Service port (8000), ONVIF port (80).
+DAHUA - Port numbers: TCP port (37777), UDP port (37778), http port (80), RTSP (554), HTTPS (443), ONVIF (default is closed, 80).
+TOPSEE - Port numbers: http (80), data port (8091) RTSP port (554) ONVIF port (80).
+JOVISION - 8554
+GWELL/YOOSEE - 554, ONVIF port: 5000; open port: 3702
+V380 - 8899
+UNIVIEW - Port numbers: HTTP (80), RTSP (554), HTTPS (110/443), ONVIF port (80)
+TIANDY - ONVIF port number (8080)
+
+554 - "RTSP"
+8000 - service port
+8091 - data port
+8899 - "ONVIF"
---
+++ webcam.sh
@@ -0,0 +1,138 @@
+#!/bin/bash
+echo ""
+echo ""
+echo "==================================================="
+echo "This is script for scaning ips working with ip-cam"
+echo "==================================================="
+echo ""
+echo "1) To run the script, install nmap. If not, maybe sudo apt install nmap ?
+2) Run as root or disable password for root."
+echo ""
+echo "Recomended config:"
+echo ""
+echo "      554 - RTSP
+      8000 - service port
+      8091 - data port
+      8899 - ONVIF1"
+
+function dow {
+	echo -ne '#####                                              (10%)\r'
+	sleep 0.5
+	echo -ne '##########                                         (20%)\r'
+	sleep 0.5
+	echo -ne '###############                                    (30%)\r'
+	sleep 0.5
+	echo -ne '####################                               (40%)\r'
+	sleep 0.5
+	echo -ne '#########################                          (50%)\r'
+	sleep 0.5
+			}
+
+echo ""
+read -p "Input 1 or 4 ports, if you want to enter more than four ports, click 'more': " a
+echo ""
+
+if [ "$a" == 1 ]; then 
+	read -p "Specify port number: " q
+		echo ""
+		echo "Scan port $q started"
+		echo ""
+		echo "Ip-addresses are stored in a file result.txt"
+		time=$(date +%s)
+		echo ""
+		dow
+		echo ""
+		
+		nmap -iL ips.txt -p $q --open > done.txt
+
+elif [ "$a" == 2 ]; then
+	read -p "Specify the first port number: " w
+	read -p "Specify the number of the second port: " e
+		echo ""
+		echo "Scan ports $w and $e started"
+		echo ""
+		echo "Ip-addresses are stored in a file result.txt"
+		time=$(date +%s)
+		echo ""
+		dow
+		echo ""
+		nmap -iL ips.txt -p $w $e --open > done.txt
+	
+elif [ "$a" == 3 ]; then
+	read -p "Specify the first port number: " a
+	read -p "Specify the number of the second port: " s
+	read -p "Specify the number of the third port: " d
+		echo ""
+		echo "Scan ports $a , $s and $d started"
+		echo ""
+		echo "Ip-addresses are stored in a file result.txt"
+		time=$(date +%s)
+		echo ""
+		dow
+		echo ""
+		
+		nmap -iL ips.txt -p $a $s $d --open > done.txt
+		
+
+elif [ "$a" == 4 ]; then
+	read -p "Specify the first port number: " z
+	read -p "Specify the number of the second port: " x
+	read -p "Specify the number of the third port: " c
+	read -p "Specify the fourth port number: " v
+		echo ""
+		echo "Scan ports $z, $x, $c and $v started"
+		echo ""
+		echo "Ip-addresses are stored in a file result.txt"	
+		time=$(date +%s)
+		echo ""
+		dow
+		echo ""
+		echo ""
+		nmap -iL ips.txt -p $z $x $c $v --open > done.txt	
+			echo ""
+			dow
+			echo ""
+
+elif [ "$a" == more ]; then 
+	read -p "Enter the number of ports: " select
+	echo ""
+		ports=()
+			for (( i = 0; i < $select; i++ )); do
+				read -p "Enter port number: " input
+		ports[$i]=$input
+			done
+	echo ""
+	echo "Scan ports ${ports[*]} started"
+	time=$(date +%s)
+	echo ""
+	echo "Ip-addresses are stored in a file result.txt"
+	echo ""
+	dow
+	echo ""
+	nmap -iL ips.txt -p ${ports[*]} --open > done.txt
+	
+fi
+
+echo -ne '##############################                     (60%)\r'
+sleep 1
+echo -ne '###################################                (70%)\r'
+sleep 0.5
+echo -ne '########################################           (80%)\r'
+sleep 0.2
+
+grep -o '[0-9]\{1,3\}\.[0-9]\{1,3\}\.[0-9]\{1,3\}\.[0-9]\{1,3\}' done.txt > result.txt
+
+echo -ne '#############################################      (90%)\r'
+sleep 0.2
+
+sudo rm done.txt
+
+echo -ne '##################################################(100%)\r'
+echo -ne '\n'
+echo ""
+echo "Result:"
+echo ""
+cat result.txt | awk '{split($0,a,":"); print a[1]}' | sort -u
+echo ""
+echo "Scan was running $(($(date +%s)-$time)) second."
+echo ""
