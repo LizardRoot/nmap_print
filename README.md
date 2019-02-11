e2cb73ab91aa754a6fe379ff84e22d04df6d8013
YSIM <ysim@support@vk.com>
2019-02-11 20:48:28 +0300

    release 3

--- README.md
+++ README.md
@@ -6,8 +6,8 @@ In fact, you can use any ports.
 
 For run script:
 1) Create the file "ips.txt".
-2) Write in "ips.txt" addresses in the format CIDR (ip/masc).
-3) Download if not nmap, grep, awk. Maybe sudo apt install nmap, grep, awk ?
+2) Write in "ips.txt" addresses in the format CIDR (ip/mask).
+3) Required: bash , nmap , grep , awk/
 
 Functional:
 After entering the number of ports from 1 to 4, if you want to use more ports, enter “+”. If you entered "+", specify the number of ports to scan, then specify the port numbers. The result will be the work will be in the terminal and in the file that you specified at the beginning.
--- webcam.sh
+++ webcam.sh
@@ -5,91 +5,233 @@ echo "==================================================="
 echo "This is script for scaning ips working with ip-cam"
 echo "==================================================="
 echo ""
-echo "1) To run the script, install nmap. If not, maybe sudo apt install nmap ?
-2) Run as root or disable password for root."
 echo ""
 echo "Recomended config:"
 echo ""
-echo "      554 - RTSP
+echo "      554  - RTSP
       8000 - service port
       8091 - data port
-      8899 - ONVIF1"
+      8899 - ONVIF"
 
-function dow {
-	echo -ne '#####                                              (10%)\r'
-	sleep 0.5
-	echo -ne '##########                                         (20%)\r'
-	sleep 0.5
-	echo -ne '###############                                    (30%)\r'
-	sleep 0.5
-	echo -ne '####################                               (40%)\r'
-	sleep 0.5
-	echo -ne '#########################                          (50%)\r'
-	sleep 0.5
-			}
 echo ""
 read -p "Save the result to: " result
 echo ""
-read -p "Input 1 or 4 ports, if you want to enter more than four ports, click 'more': " a
+
+while [[ $result = "$1" ]]; do
+	echo "Error. Incorrect input."
+	echo "No argument supplied."
+	echo ""
+	echo "Save the result to: " 
+	read result
+done
+
+echo ""
+read -p "Input 1 or 4 ports, if you want to enter more than four ports, click '+': " a
 echo ""
 
+    	while [[ ! ${a} =~ ^[1-4,"+"]+$ ]]; do
+    	echo ""
+    	echo "Error. Incorrect input."
+        echo "Please enter the number of ports you want to scan (1, 2, 3, 4) , "
+        echo "if you want to scan more than 4 ports, enter '+' :"
+        read a
+    	done
+
 if [ "$a" == 1 ]; then 
-	read -p "Specify port number (without spaces): " q
+	read -p "Specify port number: " q
+
+    	while [[ ! ${q} =~ ^[0-9]+$ ]]; do
+    	echo ""
+    	echo "Error. Incorrect input."
+        echo "Please enter number of port:"
+        read q
+    	done
+  
+	echo "Scan port $q started"
+	echo "Ip-addresses are stored in a file: $result"
+	time=$(date +%s)
+	echo ""
+
+		echo -n "Working."
+		timer_func() {
+		while sleep 1; do 
+			echo -n "." >&2; 
+		done
+		}
+		timer_func &
+		timer_func_pid=$! 
+
+	nmap -iL ips.txt -p $q --open > done.txt
+
+		trap "kill $timer_func_pid" EXIT
 		echo ""
-		echo "Scan port $q started"
-		echo ""
-		echo "Ip-addresses are stored in a file: $result"
-		time=$(date +%s)
-		echo ""
-		dow
-		nmap -iL ips.txt -p $q --open > done.txt
 
 elif [ "$a" == 2 ]; then
 	read -p "Specify the first port number: " w
+
+    	while [[ ! ${w} =~ ^[0-9]+$ ]]; do
+    	echo ""
+    	echo "Error. Incorrect input."
+        echo "Please enter number of port:"
+        read w
+    	done
+
 	read -p "Specify the number of the second port: " e
+
+    	while [[ ! ${e} =~ ^[0-9]+$ ]]; do
+    	echo ""
+    	echo "Error. Incorrect input."
+        echo "Please enter number of port:"
+        read e
+    	done
+
+	echo ""
+	echo "Scan ports $w and $e started"
+	echo "Ip-addresses are stored in a file: $result"
+	time=$(date +%s)
+	echo ""
+
+		echo -n "Working."
+		timer_func() {
+		while sleep 1; do 
+			echo -n "." >&2; 
+		done
+		}
+		timer_func &
+		timer_func_pid=$! 
+
+	nmap -iL ips.txt -p $w $e --open > done.txt
+
+		trap "kill $timer_func_pid" EXIT
 		echo ""
-		echo "Scan ports $w and $e started"
-		echo ""
-		echo "Ip-addresses are stored in a file: $result"
-		time=$(date +%s)
-		echo ""
-		dow
-		nmap -iL ips.txt -p $w $e --open > done.txt
 	
 elif [ "$a" == 3 ]; then
 	read -p "Specify the first port number: " a
+
+    	while [[ ! ${a} =~ ^[0-9]+$ ]]; do
+    	echo ""
+    	echo "Error. Incorrect input."
+        echo "Please enter number of port:"
+        read a
+    	done
+
 	read -p "Specify the number of the second port: " s
+
+    	while [[ ! ${s} =~ ^[0-9]+$ ]]; do
+    	echo ""
+    	echo "Error. Incorrect input."
+        echo "Please enter number of port:"
+        read s
+    	done
+
 	read -p "Specify the number of the third port: " d
-		echo ""
-		echo "Scan ports $a , $s and $d started"
-		echo ""
-		echo "Ip-addresses are stored in a file: $result"
-		time=$(date +%s)
-		echo ""
-		dow
-		nmap -iL ips.txt -p $a $s $d --open > done.txt
+
+    	while [[ ! ${d} =~ ^[0-9]+$ ]]; do
+    	echo ""
+    	echo "Error. Incorrect input."
+        echo "Please enter number of port:"
+        read d
+    	done
+
+	echo ""
+	echo "Scan ports $a , $s and $d started"
+	echo ""
+	echo "Ip-addresses are stored in a file: $result"
+	time=$(date +%s)
+	echo ""
+
+		echo -n "Working."
+		timer_func() {
+		while sleep 1; do 
+			echo -n "." >&2; 
+		done
+		}
+		timer_func &
+		timer_func_pid=$! 
+
+	nmap -iL ips.txt -p $a $s $d --open > done.txt
 		
+		trap "kill $timer_func_pid" EXIT
+		echo ""
 
 elif [ "$a" == 4 ]; then
 	read -p "Specify the first port number: " z
-	read -p "Specify the number of the second port: " x
-	read -p "Specify the number of the third port: " c
-	read -p "Specify the fourth port number: " v
-		echo ""
-		echo "Scan ports $z, $x, $c and $v started"
-		echo ""
-		echo "Ip-addresses are stored in a file: $result"	
-		time=$(date +%s)
-		echo ""
-		dow
-		nmap -iL ips.txt -p $z $x $c $v --open > done.txt	
 
-elif [ "$a" == more ]; then 
+    	while [[ ! ${z} =~ ^[0-9]+$ ]]; do
+    	echo ""
+    	echo "Error. Incorrect input."
+        echo "Please enter number of port:"
+        read z
+    	done
+
+	read -p "Specify the number of the second port: " x
+
+    	while [[ ! ${x} =~ ^[0-9]+$ ]]; do
+    	echo ""
+    	echo "Error. Incorrect input."
+        echo "Please enter number of port:"
+        read x
+    	done
+
+	read -p "Specify the number of the third port: " c
+
+    	while [[ ! ${c} =~ ^[0-9]+$ ]]; do
+    	echo ""
+    	echo "Error. Incorrect input."
+        echo "Please enter number of port:"
+        read c
+    	done
+
+	read -p "Specify the fourth port number: " v
+
+    	while [[ ! ${v} =~ ^[0-9]+$ ]]; do
+    	echo ""
+    	echo "Error. Incorrect input."
+        echo "Please enter number of port:"
+        read v
+    	done
+
+	echo ""
+	echo "Scan ports $z, $x, $c and $v started"
+	echo "Ip-addresses are stored in a file: $result"	
+	time=$(date +%s)
+	echo ""
+
+		echo -n "Working."
+		timer_func() {
+		while sleep 1; do 
+			echo -n "." >&2; 
+		done
+		}
+		timer_func &
+		timer_func_pid=$! 
+
+	nmap -iL ips.txt -p $z $x $c $v --open > done.txt	
+
+		trap "kill $timer_func_pid" EXIT
+		echo ""
+
+elif [ "$a" == "+" ]; then 
 	read -p "Enter the number of ports: " select
+
+		while [[ ! ${select} =~ ^[0-9]+$ ]]; do
+		echo ""
+		echo "Error. Incorrect input."
+        echo "Enter the number of ports:"
+        read select
+    	done
+
 	echo ""
 		ports=()
 			for (( i = 0; i < $select; i++ )); do
-				read -p "Enter port number: " input
+				read -p "Enter number of port : " input
+
+    				while [[ ! ${input} =~ ^[0-9]+$ ]]; do
+    				echo "Error. Incorrect input."
+        			echo "Please enter number of port:"
+        			read input
+    				done
+					
 		ports[$i]=$input
 			done
 	echo ""
@@ -98,27 +240,23 @@ elif [ "$a" == more ]; then
 	echo ""
 	echo "Ip-addresses are stored in a file: $result"
 	echo ""
-	dow
+	echo -n "Working."
+	timer_func() {
+		while sleep 1; do 
+			echo -n "." >&2; 
+		done
+		}
+		timer_func &
+		timer_func_pid=$! 
+
 	nmap -iL ips.txt -p ${ports[*]} --open > done.txt
-	
+
+		trap "kill $timer_func_pid" EXIT
+	echo ""
 fi
 
-echo -ne '##############################                     (60%)\r'
-sleep 1
-echo -ne '###################################                (70%)\r'
-sleep 0.5
-echo -ne '########################################           (80%)\r'
-sleep 0.2
-
 grep -o '[0-9]\{1,3\}\.[0-9]\{1,3\}\.[0-9]\{1,3\}\.[0-9]\{1,3\}' done.txt > $result
-
-echo -ne '#############################################      (90%)\r'
-sleep 0.2
-
 sudo rm done.txt
-
-echo -ne '##################################################(100%)\r'
-echo -ne '\n'
 echo ""
 echo "Result:"
 echo ""
